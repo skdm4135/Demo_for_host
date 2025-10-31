14d0e4df2683856e3feec612bcfcf3c585f5117e
Master Sumit <skdm4135@gmail.com>
2025-11-01 01:25:50 +0530

    Added Weather App and Configured Rest Template

---
+++ src/main/java/LEARNING/TO/HOST/GIT/DemoApi/WeatherApp.java
@@ -0,0 +1,66 @@
+package LEARNING.TO.HOST.GIT.DemoApi;
+
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+public class WeatherApp {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	private static final Logger logger = LoggerFactory.getLogger(WeatherApp.class);
+	
+	@GetMapping("/getWeather")
+	public Map<String,Object> getWeatherDetails(){
+		
+		
+		logger.info("====== ENTERED INTO WEATHER API ======");
+		
+		String baseUrl = "http://api.weatherapi.com/v1/current.json";
+		
+		String url = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                              .queryParam("key","ba001fdbe8fd4416842192617253110")
+                              .queryParam("city","Hyderabad")
+                              .toUriString();
+		
+		logger.info("==== weather url ::::: " + url);
+		
+		JSONObject finalResponse = new JSONObject();
+		
+		try {
+			
+			ResponseEntity<String> response = restTemplate.exchange
+					(url,HttpMethod.GET,null,String.class);
+			
+			logger.info("=== STATUS CODE FROM WEATHER API ::::::::::: " + response.getStatusCode());
+			
+			logger.info("==== RESPONSE COMING FROM THIRD PARTY URL ::::: "  +  response.getBody());
+			
+			
+			finalResponse.put("Resposne", response.getBody());
+			finalResponse.put("StatusCode", response.getStatusCode());
+			
+		}catch(Exception e) {
+			
+			logger.info("==== ERROR RESPONSE WHILE HITTING 3RD PARTY URL :::: " + e);
+			
+			
+			finalResponse.put("Response", "Error Received :: " + e.getMessage());
+			finalResponse.put("StatusCode", "99");
+		}
+		
+		return finalResponse.toMap();
+		
+	}
+
+}
--- src/main/java/LEARNING/TO/HOST/GIT/GitApplication.java
+++ src/main/java/LEARNING/TO/HOST/GIT/GitApplication.java
@@ -2,6 +2,8 @@ package LEARNING.TO.HOST.GIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GitApplication {
@@ -10,4 +12,8 @@ public class GitApplication {
 		SpringApplication.run(GitApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
