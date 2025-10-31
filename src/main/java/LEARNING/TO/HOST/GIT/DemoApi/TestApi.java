a62ceadecd254689ee17a68960e4b5a416cc8320
Master Sumit <skdm4135@gmail.com>
2025-11-01 00:14:07 +0530

    Added Json for Response

--- src/main/java/LEARNING/TO/HOST/GIT/DemoApi/TestApi.java
+++ src/main/java/LEARNING/TO/HOST/GIT/DemoApi/TestApi.java
@@ -7,8 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestApi {
 	
 	@GetMapping("/getName")
-	public String getName() {
-		return "Sumit Yadav";
+	public Object getName() {
+		
+		String s = "{\r\n"
+				+ "  \"Christopher Nolan\": \"Inception\",\r\n"
+				+ "  \"Quentin Tarantino\": \"Pulp Fiction\",\r\n"
+				+ "  \"Alfred Hitchcock\": \"Psycho\",\r\n"
+				+ "  \"Steven Spielberg\": \"Jurassic Park\",\r\n"
+				+ "  \"Greta Gerwig\": \"Barbie\",\r\n"
+				+ "  \"Martin Scorsese\": \"Goodfellas\",\r\n"
+				+ "  \"Denis Villeneuve\": \"Dune\",\r\n"
+				+ "  \"Stanley Kubrick\": \"2001: A Space Odyssey\",\r\n"
+				+ "  \"Jordan Peele\": \"Get Out\",\r\n"
+				+ "  \"Hayao Miyazaki\": \"Spirited Away\"\r\n"
+				+ "}";
+		return s;
 	}
 
 }
